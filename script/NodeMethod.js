fc6fc7b15414d2757da5f41eef97b9f4241f1864
jkonvicka <konvicka.g630@gmail.com>
2022-08-28 12:23:35 +0200

    [#2] Add method type

--- script/NodeMethod.js
+++ script/NodeMethod.js
@@ -3,7 +3,7 @@
 export class NodeMethod{
     constructor(name, type, visibility, parameters){
         this.name = name;
-        this.type = type;
+        this.type = type;//type
         this.visibility = visibility;
         this.parameters = parameters;
     }
